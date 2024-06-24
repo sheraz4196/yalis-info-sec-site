0ad372e436ef2a441bb1cd559a209ee3389fff63
sheraz4196 <sheraztoor321@gmail.com>
2024-06-24 13:16:11 +0500

    chore: put recaptcha key inside env

--- src/components/common/GoogleRecaptcha.jsx
+++ src/components/common/GoogleRecaptcha.jsx
@@ -4,13 +4,12 @@ import dynamic from "next/dynamic";
 const ReCAPTCHA = dynamic(() => import("react-google-recaptcha"), {
   ssr: false,
 });
-
+const siteKey = process.env.NEXT_PUBLIC_GOOGLE_RECAPTCHA_KEY;
 export default function GoogleRecaptcha({ onChange }) {
   return (
     <div className="w-max mx-auto recaptcha">
       <ReCAPTCHA
-        // sitekey="6Lfhwp4pAAAAAMG1qc3I8vRn1psTUDexB_UBHzFa"
-        sitekey="6Lem2bspAAAAAPGMzs06TKvpbXDspXUuGUg7PPtK"
+        sitekey={siteKey}
         onChange={onChange}
         scriptProps={{
           defer: true,
