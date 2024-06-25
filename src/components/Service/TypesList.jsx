9502c4d7e445d20f7e9b06d4e49dc22691e13844
sheraz4196 <sheraztoor321@gmail.com>
2024-06-25 10:42:09 +0500

    refactor: code quality

--- src/components/Service/TypesList.jsx
+++ src/components/Service/TypesList.jsx
@@ -6,43 +6,41 @@ import Image from "next/image";
 
 export default function TypesList({ data }) {
   const [selectedId, setSelectedId] = useState(0);
-  const [content, setContent] = useState(
-    data?.find((item, index) => index === 0) || {}
-  );
+  const [content, setContent] = useState(data?.[0] || {});
 
   const handleSelectId = (id) => {
     setSelectedId(id);
-    setContent(data?.find((item, index) => index === id));
+    setContent(data?.[id] || {});
   };
 
   return (
     <>
       <div className="flex gap-1.5">
-        {data?.map((item, index) => {
-          return (
-            <div
-              key={index}
-              className={`${
-                selectedId === index
-                  ? "bg-white"
-                  : "bg-zinc-100 hover:bg-cyan-100"
-              } w-full shadow-[0px_-8px_52px_rgba(50,68,80,0.1)] rounded-t-[8px] flex flex-col justify-center items-center cursor-pointer text-lg leading-[1.3] min-h-16 py-5 px-[15px] transition-[background-color 0.25s ease]`}
-              onClick={() => handleSelectId(index)}
-            >
-              <div className="mb-5">
+        {data?.map((item, index) => (
+          <div
+            key={index}
+            className={`${
+              selectedId === index
+                ? "bg-white"
+                : "bg-zinc-100 hover:bg-cyan-100"
+            } w-full shadow-[0px_-8px_52px_rgba(50,68,80,0.1)] rounded-t-[8px] flex flex-col justify-center items-center cursor-pointer text-lg leading-[1.3] min-h-16 py-5 px-[15px] transition-[background-color 0.25s ease]`}
+            onClick={() => handleSelectId(index)}
+          >
+            <div className="mb-5">
+              {item?.fields?.icon?.fields?.file?.url && (
                 <Image
-                  src={item?.fields?.icon?.fields?.file?.url}
+                  src={item.fields.icon.fields.file.url}
                   alt="icon"
                   width={80}
                   height={80}
                 />
-              </div>
-              <p className="font-extrabold text-xl text-blue">
-                {item?.fields?.title}
-              </p>
+              )}
             </div>
-          );
-        })}
+            <p className="font-extrabold text-xl text-blue">
+              {item?.fields?.title}
+            </p>
+          </div>
+        ))}
       </div>
       <div className="rounded-b-lg shadow-[0px_15px_52px_rgba(50,68,80,0.14)] bg-white py-16 w-full">
         <div className="w-[90%] mx-auto">
