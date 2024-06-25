cf054f63a1c12b2e5f3fcd7cf2836375a89b89ab
sheraz4196 <sheraztoor321@gmail.com>
2024-06-25 10:15:40 +0500

    refactor: improve code

--- src/components/Service/GetSectionList.jsx
+++ src/components/Service/GetSectionList.jsx
@@ -6,62 +6,58 @@ import Image from "next/image";
 
 export default function GetSectionList({ data }) {
   const [selectedId, setSelectedId] = useState(0);
-  const [content, setContent] = useState(
-    data?.find((item, index) => index === 0) || {}
-  );
+  const [content, setContent] = useState(data?.[0] || {});
 
   const handleSelectId = (id) => {
     setSelectedId(id);
-    setContent(data?.find((item, index) => index === id));
+    setContent(data?.[id]);
   };
 
   return (
     <>
       <div className="flex flex-row-reverse gap-1.5">
-        {data?.map((item, index) => {
-          return (
+        {data?.map((item, index) => (
+          <div
+            key={index}
+            className={`${
+              selectedId === index
+                ? "bg-blue cursor-default"
+                : "bg-zinc-50 cursor-pointer"
+            } w-full text-white rounded-t flex flex-col justify-center items-center text-lg min-h-16 p-2.5 text-center transition-[background-color_0.15s_ease-out]`}
+            style={{
+              boxShadow:
+                "0 5px 15px 0 rgba(160,160,160,0.2), 0 15px 35px 0 rgba(50,50,93,0.2)",
+            }}
+            onClick={() => handleSelectId(index)}
+          >
             <div
-              key={index}
-              className={`${
-                selectedId === index
-                  ? "bg-blue cursor-default"
-                  : "bg-zinc-50 cursor-pointer"
-              } w-full text-white rounded-t flex flex-col justify-center items-center text-lg min-h-16 p-2.5 text-center transition-[background-color_0.15s_ease-out]`}
+              className={`w-max mx-auto rounded-[50px] text-xs mb-2 py-0.5 px-2.5 transition-[all_0.15s_ease-in-out]`}
               style={{
-                boxShadow:
-                  "0 5px 15px 0 rgba(160,160,160,0.2), 0 15px 35px 0 rgba(50,50,93,0.2)",
+                backgroundColor:
+                  selectedId === index
+                    ? item?.fields?.serviceActiveBgColor ||
+                      item?.fields?.serviceBgColor
+                    : item?.fields?.serviceBgColor,
+                color:
+                  selectedId === index
+                    ? item?.fields?.serviceActiveColor || "white"
+                    : "white",
               }}
-              onClick={() => handleSelectId(index)}
             >
-              <div
-                className={`w-max mx-auto rounded-[50px] text-xs mb-2 py-0.5 px-2.5 transition-[all_0.15s_ease-in-out]`}
-                style={{
-                  backgroundColor:
-                    selectedId === index
-                      ? item?.fields?.serviceActiveBgColor ||
-                        item?.fields?.serviceBgColor
-                      : item?.fields?.serviceBgColor,
-                  color:
-                    selectedId === index
-                      ? item?.fields?.serviceActiveColor || "white"
-                      : "white",
-                }}
-              >
-                {item?.fields?.service}
-              </div>
-              <p
-                className={`text-lg leading-[1.3] ${
-                  selectedId === index ? "text-white" : "text-blue"
-                }`}
-              >
-                {item?.fields?.shortTitle}
-              </p>
-              <p className="text-slate-40 text-lg">
-                {item?.fields?.shortSubHeading}
-              </p>
+              {item?.fields?.service}
             </div>
-          );
-        })}
+            <p
+              className={`text-lg leading-[1.3] ${
+                selectedId === index ? "text-white" : "text-blue"
+              }`}
+            >
+              {item?.fields?.shortTitle}
+            </p>
+            <p className="text-slate-40 text-lg">
+              {item?.fields?.shortSubHeading}
+            </p>
+          </div>
+        ))}
       </div>
       <div
         className="rounded-b-sm bg-white py-16 w-full z-10 relative"
@@ -70,7 +66,7 @@ export default function GetSectionList({ data }) {
             "0 5px 15px 0 rgba(0,0,0,0.07), 0 15px 35px 0 rgba(0,0,0,0.07)",
         }}
       >
-        <div className="mb-5 w-4/5  mx-auto flex items-center justify-between gap-10">
+        <div className="mb-5 w-4/5 mx-auto flex items-center justify-between gap-10">
           <div className="w-full">
             <h3 className="text-blue text-3xl">{content?.fields?.title}</h3>
             <p className="text-slate-40 text-2xl mb-2.5 font-extrabold">
@@ -94,13 +90,11 @@ export default function GetSectionList({ data }) {
         <div className="w-[90%] mx-auto">
           {content?.fields?.list?.length > 0 && (
             <ul className="list-checked types-list mb-5 columns-2">
-              {content?.fields?.list?.map((item, index) => {
-                return (
-                  <li key={index} className="text-lg">
-                    {item}
-                  </li>
-                );
-              })}
+              {content?.fields?.list?.map((item, index) => (
+                <li key={index} className="text-lg">
+                  {item}
+                </li>
+              ))}
             </ul>
           )}
           {content?.fields?.buttonText && (
