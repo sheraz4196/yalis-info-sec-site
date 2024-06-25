10b4953e084056d80d910c98c0770a9c4cfce746
sheraz4196 <sheraztoor321@gmail.com>
2024-06-25 10:18:05 +0500

    refactor: code refactor

--- src/components/Service/MethodologySection.jsx
+++ src/components/Service/MethodologySection.jsx
@@ -8,35 +8,35 @@ export default function MethodologySection({ data }) {
   const [isVisible, setIsVisible] = useState([]);
 
   useEffect(() => {
-    const liObservers = data?.list?.map((_, index) => {
-      return new IntersectionObserver(
-        ([entry]) => {
-          const updatedVisibility = [...isVisible];
-          if (entry.isIntersecting && !isVisible[index]) {
+    const observers = [];
+
+    const handleIntersection = (entries, observer) => {
+      entries.forEach((entry) => {
+        const index = liRefs.current.indexOf(entry.target);
+        if (entry.isIntersecting && !isVisible[index]) {
+          setIsVisible((prevState) => {
+            const updatedVisibility = [...prevState];
             updatedVisibility[index] = true;
-            setIsVisible(updatedVisibility);
-          }
-        },
-        {
+            return updatedVisibility;
+          });
+        }
+      });
+    };
+
+    liRefs.current.forEach((ref, index) => {
+      if (ref) {
+        const observer = new IntersectionObserver(handleIntersection, {
           root: null,
           rootMargin: "0px",
           threshold: 0.3,
-        }
-      );
-    });
-
-    data?.list?.forEach((_, index) => {
-      if (liRefs.current[index]) {
-        liObservers[index].observe(liRefs.current[index]);
+        });
+        observer.observe(ref);
+        observers.push(observer);
       }
     });
 
     return () => {
-      data?.list?.forEach((_, index) => {
-        if (liRefs.current[index]) {
-          liObservers[index].unobserve(liRefs.current[index]);
-        }
-      });
+      observers.forEach((observer) => observer.disconnect());
     };
   }, [data?.list, isVisible]);
 
@@ -46,37 +46,31 @@ export default function MethodologySection({ data }) {
       {data?.list?.length > 0 && (
         <div className="methodology-list relative px-5 lg:px-0">
           <ol>
-            {data?.list?.map((item, i) => {
-              const liRef = (element) => {
-                liRefs.current[i] = element;
-              };
-
-              return (
-                <li
-                  ref={liRef}
-                  key={i}
-                  className={`${isVisible[i] ? "on" : ""}`}
-                >
-                  <div>
-                    {item?.fields?.icon?.fields?.file?.url && (
-                      <Image
-                        src={item?.fields?.icon?.fields?.file?.url}
-                        alt="icon"
-                        width={130}
-                        height={130}
-                        className="max-w-16 lg:max-w-[130px] mx-auto"
-                      />
-                    )}
-                    <h3 className="text-lg text-blue mb-1.5">
-                      {item?.fields?.title}
-                    </h3>
-                    <div className="rich-text">
-                      <Richtext data={item?.fields?.description} />
-                    </div>
+            {data?.list?.map((item, i) => (
+              <li
+                ref={(el) => (liRefs.current[i] = el)}
+                key={i}
+                className={`${isVisible[i] ? "on" : ""}`}
+              >
+                <div>
+                  {item?.fields?.icon?.fields?.file?.url && (
+                    <Image
+                      src={item.fields.icon.fields.file.url}
+                      alt="icon"
+                      width={130}
+                      height={130}
+                      className="max-w-16 lg:max-w-[130px] mx-auto"
+                    />
+                  )}
+                  <h3 className="text-lg text-blue mb-1.5">
+                    {item?.fields?.title}
+                  </h3>
+                  <div className="rich-text">
+                    <Richtext data={item?.fields?.description} />
                   </div>
-                </li>
-              );
-            })}
+                </div>
+              </li>
+            ))}
           </ol>
         </div>
       )}
