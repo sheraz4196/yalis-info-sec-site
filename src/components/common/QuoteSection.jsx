a308277f93d3020a0cb660bf3228beb91c8936c2
sheraz4196 <sheraztoor321@gmail.com>
2024-06-26 10:18:14 +0500

    fix: background colors

--- src/components/common/QuoteSection.jsx
+++ src/components/common/QuoteSection.jsx
@@ -3,7 +3,7 @@ import ContactForm from "../contact/contact-form";
 
 export default function QuoteSection({ title, description }) {
   return (
-    <section className="bg-muted-red-brown" id="getQuote">
+    <section className="bg-muted-brown mt-4" id="getQuote">
       <div className="max-container py-10 md:py-16">
         <h2 className="text-white text-center mb-2.5">{title}</h2>
         <p className="text-gray-light text-lg mb-2.5 text-center">
