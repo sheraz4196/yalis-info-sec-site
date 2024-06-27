7a99eb5d285058d2336b0626e7b704c4e11123e1
sheraz4196 <sheraztoor321@gmail.com>
2024-06-27 16:55:46 +0500

    fix: ui not rendering

--- src/components/Service/GetSectionList.jsx
+++ src/components/Service/GetSectionList.jsx
@@ -17,46 +17,55 @@ export default function GetSectionList({ data }) {
     <>
       <div className="flex flex-row-reverse gap-1.5">
         {data?.map((item, index) => (
-          <div
-            key={index}
-            className={`${
-              selectedId === index
-                ? "bg-blue cursor-default"
-                : "bg-zinc-50 cursor-pointer"
-            } w-full text-white rounded-t flex flex-col justify-center items-center text-lg min-h-16 p-2.5 text-center transition-[background-color_0.15s_ease-out]`}
-            style={{
-              boxShadow:
-                "0 5px 15px 0 rgba(160,160,160,0.2), 0 15px 35px 0 rgba(50,50,93,0.2)",
-            }}
-            onClick={() => handleSelectId(index)}
-          >
-            <div
-              className={`w-max mx-auto rounded-[50px] text-xs mb-2 py-0.5 px-2.5 transition-[all_0.15s_ease-in-out]`}
-              style={{
-                backgroundColor:
+          <React.Fragment key={index}>
+            {item.fields && (
+              <div
+                className={`${
                   selectedId === index
-                    ? item?.fields?.serviceActiveBgColor ||
-                      item?.fields?.serviceBgColor
-                    : item?.fields?.serviceBgColor,
-                color:
-                  selectedId === index
-                    ? item?.fields?.serviceActiveColor || "white"
-                    : "white",
-              }}
-            >
-              {item?.fields?.service}
-            </div>
-            <p
-              className={`text-lg leading-[1.3] ${
-                selectedId === index ? "text-white" : "text-blue"
-              }`}
-            >
-              {item?.fields?.shortTitle}
-            </p>
-            <p className="text-slate-40 text-lg">
-              {item?.fields?.shortSubHeading}
-            </p>
-          </div>
+                    ? "bg-blue cursor-default"
+                    : "cursor-pointer bg-zinc-200"
+                } w-full text-white rounded-t flex flex-col justify-center items-center text-lg min-h-16 p-2.5 text-center transition-[background-color_0.15s_ease-out]`}
+                style={{
+                  boxShadow:
+                    "0 5px 15px 0 rgba(160,160,160,0.2), 0 15px 35px 0 rgba(50,50,93,0.2)",
+                }}
+                onClick={() => handleSelectId(index)}
+              >
+                {item?.fields?.service && (
+                  <div
+                    className={`w-max mx-auto rounded-[50px] text-xs mb-2 py-0.5 px-2.5 transition-[all_0.15s_ease-in-out]`}
+                    style={{
+                      backgroundColor:
+                        selectedId === index
+                          ? item?.fields?.serviceActiveBgColor ||
+                            item?.fields?.serviceBgColor
+                          : item?.fields?.serviceBgColor,
+                      color:
+                        selectedId === index
+                          ? item?.fields?.serviceActiveColor || "white"
+                          : "white",
+                    }}
+                  >
+                    {item?.fields?.service}
+                  </div>
+                )}
+                {item?.fields?.shortTitle && (
+                  <p
+                    className={`text-lg leading-[1.3] ${
+                      selectedId === index ? "text-white" : "text-blue"
+                    }`}
+                  >
+                    {item?.fields?.shortTitle}
+                  </p>
+                )}
+                {item?.fields?.shortSubHeading && (
+                  <p className="text-slate-40 text-lg">
+                    {item?.fields?.shortSubHeading}
+                  </p>
+                )}
+              </div>
+            )}
+          </React.Fragment>
         ))}
       </div>
       <div
